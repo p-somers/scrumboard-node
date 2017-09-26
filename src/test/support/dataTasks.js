7986ad096b96cd0c6d17f090f3774b2b0bfd8c27
peter.somers <petersomers2@gmail.com>
2017-09-26 15:35:57 -0400

    Implemented prepareData() function in dataTasks, which scans the support/data folder for json files with test data and imports them into mongodb based on file name -> table

--- src/test/support/dataTasks.js
+++ src/test/support/dataTasks.js
@@ -1,8 +1,24 @@
+const fs = require('fs');
 const MongoClient = require('mongodb').MongoClient;
 const tables = ['users', 'companies', 'teams', 'burndowns', 'tasks', 'stories'];
+const dataDir = __dirname + '/data';
 
 module.exports = async function(url) {
     let db = await MongoClient.connect(url);
+
+    async function importFile(filename) {
+        return new Promise(resolve => {
+            let table = filename.split('.')[0];
+            let type = filename.split('.')[1];
+            if (tables.includes(table)) {
+                let data = require(dataDir + '/' + filename);
+                db.collection(table, (error, collection) => {
+                    collection.insertMany(data).then(resolve);
+                });
+            }
+        });
+    }
+
     return {
         async deleteData() {
             return Promise.all(tables.map(table => {
@@ -10,7 +26,15 @@ module.exports = async function(url) {
             }));
         },
         async prepareData() {
-
+            return new Promise(resolve => {
+                fs.readdir(dataDir, function(error, files) {
+                    if (error) {
+                        console.error('error reading test data directory', error);
+                    } else {
+                        Promise.all(files.map(filename => {return importFile(filename);})).then(resolve);
+                    }
+                });
+            })
         }
     }
 }
