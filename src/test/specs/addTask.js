8018f40128165ecc2868eab3302865fe91779d2e
peter.somers <petersomers2@gmail.com>
2017-10-11 16:52:19 -0400

    preliminary check-in for "add task" test

---
+++ src/test/specs/addTask.js
@@ -0,0 +1,33 @@
+require('../support/context');
+const HomePage = require('../support/elements/pages/HomePage');
+const AddTaskModal = require('../support/elements/modal/Task');
+
+const assert = require('chai').assert;
+
+suite('Edit Story', function () {
+    let board;
+    before(function() {
+        login('test', 'password');
+        board = new HomePage(browser).getBoard();
+    });
+
+    describe('when a user clicks the "add task" button', function() {
+        let addTaskModal, story, task;
+        before(function() {
+            story = board.getStoryByName('Test Task');
+            story.getAddTaskButton().click();
+            addTaskModal = new AddTaskModal(browser);
+            addTaskModal.getTaskNameInput().clear().sendKeys('New Test Task');
+            addTaskModal.getHoursInput().sendKeys('16');
+            addTaskModal.getNotesInput().sendKeys('Task Notes');
+            addTaskModal.getSaveButton().click();
+            task = story.getTaskByName('Test Task');
+        });
+
+        it('should show the new task on the board', function() {
+            assert.exists(task);
+            assert.equal(task.getName(), 'Test Task');
+            assert.equal(task.getHours(), '16');
+        });
+    });
+});
